e3e7aa4c36d0c30152075840e9aab9e981eea454
dao <david.ochsner@empa.ch>
2018-10-19 08:55:57 +0200

    Added word-highlighting

--- emacs_init.el
+++ emacs_init.el
@@ -1,12 +1,15 @@
 ;; .emacs
 
 (custom-set-variables
- ;; uncomment to always end a file with a newline
- ;'(require-final-newline t)
- ;; uncomment to disable loading of "default.el" at startup
- '(inhibit-default-init t)
- ;; default to unified diffs
- '(diff-switches "-u"))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(delete-selection-mode nil)
+ '(diff-switches "-u")
+ '(highlight-symbol-idle-delay 0.3)
+ '(highlight-symbol-on-navigation-p t)
+ '(inhibit-default-init t))
 
 
 ;; INSTALL PACKAGES
@@ -24,6 +27,7 @@
   '(better-defaults
     material-theme
     fill-column-indicator
+    highlight-symbol
     ;; Manually load dependencies for elpy; need to install 's' by hand from melpa
     highlight-indentation
     yasnippet
@@ -44,6 +48,7 @@
 (global-linum-mode t) ;; enable line numbers globally
 
 ;; Manually load backported elpy package
+;; clone the repo to the path below, checkout the backport branch
 (add-to-list 'load-path "~/.emacs.d/lisp/elpy")
 (load "elpy")
 (elpy-enable)
@@ -56,3 +61,12 @@
 
 ;; Control inital window size
 (add-to-list 'initial-frame-alist '(width . 85))
+
+;; Highlight all occurences of the word under the point
+(add-hook 'prog-mode-hook #'highlight-symbol-mode)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
